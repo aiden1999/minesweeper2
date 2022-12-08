3fc626f5a90bec4677750b00933e6af3e9290e3e
Aiden <tempestaiden@gmail.com>
2022-12-08 00:50:48 +0000

    wow

--- menu.py
+++ menu.py
@@ -2,23 +2,27 @@ import tkinter as tk
 
 
 class Menu(tk.Tk):
-
+    
     def __init__(self) -> None:
 
         super().__init__()
         
         self.difficulty = tk.StringVar
+        self.title = "Minesweeper - Game Setup"
+        self.configure(background="#282a36")
         
         menu_instructions = tk.Label(self, text="Choose game options")
         menu_instructions.pack()
         rows_slider = tk.Scale(self, from_=5, to=100, orient="horizontal",
-                               length=400, label="Choose the number of rows")
+                               length=400, label="Rows", background="#282a36",
+                               foreground="#f8f8f2")
         rows_slider.pack()
         cols_slider = tk.Scale(self, from_=5, to=100, orient="horizontal",
-                               length=400, label="Choose the number of columns")
+                               length=400, label="Columns", background="#282a36",
+                               foreground="#f8f8f2")
         cols_slider.pack()
         difficulty_frame = tk.Frame(self)
-        difficulty_instructions = tk.Label(difficulty_frame, text="Choose a difficulty")
+        difficulty_instructions = tk.Label(difficulty_frame, text="Difficulty")
         difficulty_instructions.pack()
         easy_radiobutton = tk.Radiobutton(difficulty_frame, text="Easy",
                                           value="easy", variable=self.difficulty)
@@ -34,4 +38,8 @@ class Menu(tk.Tk):
                                           value="hell", variable=self.difficulty)
         hell_radiobutton.pack(side="left")
         difficulty_frame.pack()
-        
+        ok_button = tk.Button(self, text="OK", command=self.ok_button_clicked)
+        ok_button.pack(side="left")
+
+    def ok_button_clicked():
+        print("wow")
